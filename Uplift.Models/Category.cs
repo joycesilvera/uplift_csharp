de9be9bf4dd142232f7c49c0d3eb3f3f896945fb
Joyce Silvera <60859729+joycesilvera@users.noreply.github.com>
2021-01-31 21:33:23 -0800

    added Category Model

---
+++ Uplift.Models/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+
+namespace Uplift.Models
+{
+    public class Category
+    {
+        [Key] //makes primary key which is auto-required
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name="Category Name")]
+        public string Name { get; set; }
+
+
+        [Required]
+        [Display(Name = "Display Order")]
+        public int DisplayOrder { get; set; }
+    }
+}
