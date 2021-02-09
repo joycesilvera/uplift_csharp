036db2840b6068031b6148c4e58a565e4c8628e3
Joyce Silvera <60859729+joycesilvera@users.noreply.github.com>
2021-02-08 20:32:42 -0800

    DELETE for Category, CRUD Completegit status

--- Uplift/wwwroot/js/category.js
+++ Uplift/wwwroot/js/category.js
@@ -38,3 +38,32 @@ function loadDataTable() {
         "width": "100%"
     });
 }
+
+function Delete(url) {
+    swal({
+        title: "Are you sure you want to delete?",
+        text: "You will not be able to restore the content!",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#DD6B55",
+        confirmButtonText: "Yes, delete it!",
+        closeOnconfirm: true
+    }, function () {
+        $.ajax({
+            type: 'DELETE',
+            url: url,
+            success: function (data) {
+                if (data.success) {
+                    toastr.success(data.message);
+                    dataTable.ajax.reload();
+                }
+                else {
+                    toastr.error(data.message);
+                }
+            }
+        });
+    });
+}
+
+
+    
